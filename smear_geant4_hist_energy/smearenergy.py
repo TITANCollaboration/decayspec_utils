6d17cf99460ce5ce3b00b061405230639af1f8d4
Jon Ringuette <bobskr@gmail.com>
2021-01-26 14:37:52 -0800

    Can now output csv file that is compatible with the other decayspec utils

--- smear_geant4_hist_energy/smearenergy.py
+++ smear_geant4_hist_energy/smearenergy.py
@@ -5,24 +5,29 @@
 #            so it has a more gaussian distribution and is more realistic
 # *************************************************************************************
 import argparse
-import configparser
-from array import array
 import uproot
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def process_root_file(root_file_name):
+def process_root_file(root_file_name, output_file):
     root_file = uproot.open(root_file_name)
-    initdata = root_file["EVENT_NTUPLE"]["pulse_height"].array()
+    initdata = root_file["EVENT_NTUPLE"]["pulse_height"].array().astype(int)
     newdata = gaussian_filter1d(initdata, sigma=.3)
     print(initdata)
     print(newdata)
-
+    if output_file is not None:
+        mydict = {'0': newdata}
+        print(mydict)
+        pd_particle_events = pd.DataFrame(mydict)  # convert list of dict's into pandas dataframe
+        print("Writing to CSV file :", output_file)
+        pd_particle_events.to_csv(output_file, sep='|', header=True, index=False, chunksize=50000, mode='w', encoding='utf-8')
+        exit(0)
     fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
-    axs[0].hist(initdata, bins=200)
-    axs[1].hist(newdata, bins=200)
+    axs[0].hist(initdata, histtype='step', bins=1200)
+    axs[1].hist(newdata, histtype='step', bins=1200)
     plt.show()
 
 
@@ -31,11 +36,14 @@ def main():
 
     parser.add_argument('--root_file', dest='root_file_name', required=True,
                         help="Root file to read in")
+    parser.add_argument('--output', dest='output_file', default=None, required=False,
+                        help="Name of output CSV file")
+
     parser.set_defaults(configFile="g4macsched.cfg")
 
     args, unknown = parser.parse_known_args()
     # processConfigFile(args.configFile)
-    process_root_file(args.root_file_name)
+    process_root_file(args.root_file_name, args.output_file)
 
 
 if __name__ == "__main__":
